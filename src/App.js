c0ce841325eeb4006aa97f0e96b6a90ca7fdc69a
Anshika Agarwal <anshika@Anshikas-MacBook-Pro.local>
2023-07-19 12:50:10 +0530

    React Restaurant project setup with components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,114 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+/**
+ * Header
+ *   - Restaurant Logo
+ *   - Navigation Items (Home , About, Contact)
+ * Body
+ *   - Search Bar
+ *   - Restaurant Cards
+ * Restaurant Cards
+ *   - Image of restaurant
+ *   - Name of restaurant
+ *   - Cuisines
+ *   - Rating
+ *   - Cost for one
+ *   - Time to deliver
+ * Footer
+ *   - Copyright
+ *   - Links
+ *   - About Restaurant
+ */
+
+const Header = () => {
+  return (
+    <nav className="header">
+      <img
+        className="food-logo"
+        src="https://img.freepik.com/premium-vector/good-food-logo-template_79169-17.jpg?w=2000"
+        alt="food-logo"
+      ></img>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+};
+
+const RestaurantCard = (props) => {
+  // Object destructuring
+
+  const { name, cuisines, rating } = props;
+
+  return (
+    <div className="restaurant-card">
+      <img
+        src="https://b.zmtcdn.com/data/pictures/chains/8/19803678/365eb84dced494844fc142944ec7272b_o2_featured_v2.jpg?output-format=webp"
+        alt="restaurant"
+      ></img>
+      <div className="res-card-details">
+        <div>
+          <h2>{name}</h2>
+          <span>{cuisines}</span>
+        </div>
+
+        <div>
+          <h3>{rating}</h3>
+          <span>Rs 100 for one</span>
+          <h4>41 min</h4>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const Body = () => {
+  return (
+    <>
+      <div className="search-bar">
+        <input type="text"></input>
+        <button>Search</button>
+      </div>
+      <div className="res-container">
+        <RestaurantCard
+          name="Biryani Center"
+          cuisines="IceCream, Mithai, Desserts"
+        />
+        <RestaurantCard
+          name="IceCream Parlour"
+          cuisines="biryani"
+          rating="4.3"
+        />
+        <RestaurantCard name="Roots Restaurant" cuisines="burger" />
+        <RestaurantCard name="momos corner" cuisines="momos" />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+      </div>
+    </>
+  );
+};
+
+const Footer = () => {
+  return (
+    <footer>
+      <h2>Copyright</h2>
+    </footer>
+  );
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Body />
+      <Footer />
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,10 +1,61 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const element = (
+  <div id="parent">
+    <div id="child1">
+      <h1>Hello world using React Child </h1>
+    </div>
+
+    <div id="child2">
+      <h1>Hello world using React Child2 </h1>
+    </div>
+  </div>
+);
+
+const learn = (
+  <>
+    <h1>Learning JSX</h1>
+    <h1>Learning jsx....</h1>
+  </>
+);
+
+// function AbcComponent() {
+//   return <h1>Learning Components in React</h1>;
+// }
+
+const AbcComponent = () => <h1>Learning Components in React</h1>;
+
+// Component composition
+// function XYZComponent() {
+//   return (
+//     <>
+//       <AbcComponent />
+//       <h1>XYZ component......</h1>
+//     </>
+//   );
+//}
+
+const XYZComponent = () => {
+  return (
+    <>
+      <AbcComponent />
+      <h1>XYZ component......</h1>
+    </>
+  );
+};
+
+// JSX requires one parent element
+
+// React Fragment
+
+//root.render(<XYZComponent />);
+
 root.render(
   <React.StrictMode>
     <App />
